cac31f9d2396692f2bb51640d44fd7c62012f2ef
rperez_coaat <rperez@coaat.es>
2023-03-04 11:16:05 +0100

    Ponemos el voton de volver con la misma apariencia

--- src/components/screens/ModalScreen.jsx
+++ src/components/screens/ModalScreen.jsx
@@ -16,35 +16,37 @@ class ModalScreen extends Component {
   }
   render() {
     return (
-      <View style={styles.container}>
-        <Modal
-          animationType={"slide"}
-          transparent={false}
-          visible={this.state.modalVisible}
-          onRequestClose={() => {
-            console.log("Modal has been closed.");
-          }}
-        >
-          <View style={styles.modal}>
-            <Text style={styles.text}>Modal is open!</Text>
+      <View>
+        <View style={styles.container}>
+          <Modal
+            animationType={"slide"}
+            transparent={false}
+            visible={this.state.modalVisible}
+            onRequestClose={() => {
+              console.log("Modal has been closed.");
+            }}
+          >
+            <View style={styles.modal}>
+              <Text style={styles.text}>Modal is open!</Text>
 
-            <TouchableHighlight
-              onPress={() => {
-                this.toggleModal(!this.state.modalVisible);
-              }}
-            >
-              <Text style={styles.text}>Close Modal</Text>
-            </TouchableHighlight>
-          </View>
-        </Modal>
+              <TouchableHighlight
+                onPress={() => {
+                  this.toggleModal(!this.state.modalVisible);
+                }}
+              >
+                <Text style={styles.text}>Close Modal</Text>
+              </TouchableHighlight>
+            </View>
+          </Modal>
 
-        <TouchableHighlight
-          onPress={() => {
-            this.toggleModal(true);
-          }}
-        >
-          <Text style={styles.text}>Open Modal</Text>
-        </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => {
+              this.toggleModal(true);
+            }}
+          >
+            <Text style={styles.text}>Open Modal</Text>
+          </TouchableHighlight>
+        </View>
         <Text> </Text>
         <Button
           title="Go back"
